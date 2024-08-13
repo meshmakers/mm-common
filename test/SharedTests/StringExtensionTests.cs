b610214f061aacba8e3278f10532d9ff62b70ef8
Gerald Lochner <gerald.lochner@meshmakers.io>
2024-08-13 10:20:13 +0200

    Added unit test

---
+++ test/SharedTests/StringExtensionTests.cs
@@ -0,0 +1,16 @@
+﻿using Meshmakers.Common.Shared;
+using Xunit;
+
+namespace Meshmakers.Common.SharedTests;
+
+public class StringExtensionTests
+{
+    [Fact]
+    public void NormalizeString_Ok()
+    {
+        string test = "aBc";
+        var result = test.NormalizeString();
+
+        Assert.Equal("abc", result);
+    }
+}
